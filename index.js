d768ac4d19e98a66dafa641ecdb212e8976f23ca
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-18 22:30:59 +0600

    Create 'brands.json' file and send data to the client side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const brands = require('./brands.json');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,10 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Tech Titan server is running')
+})
+
+app.get('/brands', (req, res) => {
+    res.send(brands)
 })
 
 app.listen(port, ()=> {
